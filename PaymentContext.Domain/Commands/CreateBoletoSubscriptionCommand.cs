3d420191ea2b3e828287e2dd370f6039fa359670
Eduardo Felipe <edufslima9@gmail.com>
2022-08-13 18:14:11 -0300

    feat: Creating Commands

---
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -0,0 +1,46 @@
+using System.Reflection.Metadata;
+using Flunt.Notifications;
+using Flunt.Validations;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
+
+namespace PaymentContext.Domain.Commands
+{
+  public class CreateBoletoSubscriptionCommand : Notifiable<Notification>, ICommand
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Document { get; set; }
+    public string Email { get; set; }
+
+    public string BarCode { get; set; }
+    public string BoletoNumber { get; set; }
+    public string PaymentNumber { get; set; }
+    public DateTime PaidDate { get; set; }
+    public DateTime ExpireDate { get; set; }
+    public decimal Total { get; set; }
+    public decimal TotalPaid { get; set; }
+    public string Payer { get; set; }
+    public Document PayerDocument { get; set; }
+    public EDocumentType PayerDocumentType { get; set; }
+    public string PayerEmail { get; set; }
+    public string Street { get; set; }
+    public string Number { get; set; }
+    public string Neighborhood { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string ZipCode { get; set; }
+
+    public void Validate()
+    {
+      AddNotifications(new Contract<CreatePaypalSubscriptionCommand>()
+        .Requires()
+        .IsGreaterThan(FirstName, 3, "CreatePaypalSubscriptionCommand.FirstName", "Nome deve conter pelo menos 3 caracteres")
+        .IsGreaterThan(LastName, 3, "CreatePaypalSubscriptionCommand.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+        .IsLowerThan(FirstName, 40, "CreatePaypalSubscriptionCommand.FirstName", "Nome deve conter até 40 caracteres")
+        .IsLowerThan(LastName, 40, "CreatePaypalSubscriptionCommand.LastName", "Sobrenome deve conter até 40 caracteres")
+      );
+    }
+  }
+}
