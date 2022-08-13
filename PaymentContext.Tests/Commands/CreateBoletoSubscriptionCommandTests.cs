d06d516711e6f84ae6e1930006469ee190fcbd31
Eduardo Felipe <edufslima9@gmail.com>
2022-08-13 18:14:28 -0300

    test: Testing CreateBoletoSubscriptionCommand

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,22 @@
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.ValueObjects
+{
+  [TestClass]
+  public class CreateBoletoSubscriptionCommandTests
+  {
+      // Red, Green, Refactor
+
+      [TestMethod]
+      public void ShouldReturnErrorWhenNameIsInvalid()
+      {
+        var command = new CreateBoletoSubscriptionCommand();
+        command.FirstName = "";
+
+        command.Validate();
+        Assert.AreEqual(false, command.IsValid);
+      }
+  }
+}
